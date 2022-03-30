33e736c7a581f5e7c73935d3bbc5defd34e78c77
Simon <59486823+simon2022@users.noreply.github.com>
2022-03-30 15:30:31 +0200

    test failing test

---
+++ test/de/dhbw/loadbalancer/FailingTest.java
@@ -0,0 +1,14 @@
+package de.dhbw.loadbalancer;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FailingTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
